00087ec1bea1b1b9208cece367fbc9ddea5e4443
p4welo <p4welo@gmail.com>
2014-07-17 18:07:03 +0200

    php mysql info

--- dao/news.php
+++ dao/news.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 if (mysql_connect($host, $login, $pwd) and mysql_select_db($db)) {
-    mysql_query('SET CHARACTER SET utf8')
+    mysql_query('SET CHARACTER SET utf8');
 	$result = mysql_query("SELECT * FROM news WHERE 1");
 	$rows = array();
 	while ($r = mysql_fetch_assoc($result)) {
