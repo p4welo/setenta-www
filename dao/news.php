528e252d4e02cf46aaa6fcc23386d6d9955a46f3
p4welo <p4welo@gmail.com>
2014-07-17 18:31:39 +0200

    news list descending order

--- dao/news.php
+++ dao/news.php
@@ -6,7 +6,7 @@ ini_set('display_errors', '1');
 
 if (mysql_connect($host, $login, $pwd) and mysql_select_db($db)) {
     mysql_query('SET CHARACTER SET utf8');
-	$result = mysql_query("SELECT * FROM news WHERE 1");
+	$result = mysql_query("SELECT * FROM news ORDER BY created_at DESC");
 	$rows = array();
 	while ($r = mysql_fetch_assoc($result)) {
 		$rows[] = $r;
