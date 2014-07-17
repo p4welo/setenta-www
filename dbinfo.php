311f8ba43b9131b08415687f67e2fb379d31b4dd
p4welo <p4welo@gmail.com>
2014-07-17 17:10:50 +0200

    php mysql info

---
+++ dbinfo.php
@@ -0,0 +1,6 @@
+<?php
+	$host='192.168.1.106';
+	$login='setenta_p4welo';
+	$pwd='pawelo88';
+	$db='setenta_www';
+?>
---
+++ news.php
@@ -0,0 +1,17 @@
+﻿<?php
+include 'dbinfo.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+if (mysql_connect($host, $login, $pwd) and mysql_select_db($db)) {
+	$result = mysql_query("SELECT * FROM news WHERE 1");
+	$rows = array();
+	while ($r = mysql_fetch_assoc($result)) {
+		$rows[] = $r;
+	}
+	header('Content-Type: application/json');
+	echo(json_encode($rows));
+	mysql_close();
+}
+?>
