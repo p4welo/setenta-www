aa90e9e3ea15442c0b7a3d69c593a64936a5cb03
p4welo <p4welo@gmail.com>
2014-10-10 10:11:35 +0200

    gAnalytics

--- app/app.js
+++ app/app.js
@@ -1,5 +1,3 @@
-//var SERVER_URL = "http://91.218.78.136/ap/public";
-
 define([
     'uiRouter',
     'ngAnimate',
@@ -47,65 +45,3 @@ define([
                 });
         })
 });
-
-//angular.module('setenta-app.services', ['ngResource']);
-//angular.module('setenta-app.controllers', []);
-
-//define([
-//    'uiRouter',
-//    'ngAnimate',
-//    'ngTouch',
-//    'loadingBar',
-//
-//    'services/module',
-//
-//    'customers/index',
-//    'news/index',
-//    'schedule/index',
-//    'system/index'
-//], function () {
-//
-//    return angular.module('activePortal', [
-//        'ngAnimate',
-//        'ngTouch',
-//        'ui.router',
-//        'angular-loading-bar',
-//
-//        'activePortal.system',
-//        'activePortal.schedule',
-//        'activePortal.news',
-//        'activePortal.customers',
-//        'activePortal.services',
-//
-//        'activePortal.services'
-//    ], function ($urlRouterProvider) {
-//
-//        $urlRouterProvider.otherwise("/users");
-//    })
-//        .config(function ($translateProvider) {
-//            $translateProvider.translations('pl', {
-//
-//                PN: 'Poniedziałek',
-//                WT: 'Wtorek',
-//                SR: 'Środa',
-//                CZ: 'Czwartek',
-//                PT: 'Piątek',
-//                SB: 'Sobota',
-//                ND: 'Niedziela',
-//
-//                BEGINNER: "Początkujący",
-//                INTERMEDIATE: "Średniozaawansowany",
-//                ADVANCED: "Zaawansowany",
-//                OPEN: "Otwarty",
-//
-//                m: "Mała",
-//                d: "Duża",
-//
-//                TYPE_REGISTRATION: "Grupa na zapisy",
-//                TYPE_OPEN: "Można dołączyć",
-//                TYPE_CLOSED: "Nie można dołączyć"
-//
-//            });
-//            $translateProvider.preferredLanguage('pl');
-//        });
-//});
